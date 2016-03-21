87166873e5c7f01354d6e25d50cf8b3003a23e74
Evegenn <demerbrise@gmail.com>
2016-03-21 17:13:14 +0300

    working with command line

---
+++ src/_java/_se/_01/_start/CommandArg.java
@@ -0,0 +1,12 @@
+package _java._se._01._start;
+
+public class CommandArg {
+
+	public static void main(String[] args) {
+		for (int i=0; i<args.length; i++) {
+			System.out.println("Аргумент " + i + " = " + args[i]);
+		}
+
+	}
+
+}
---
+++ src/_java/_se/_01/_start/Console.java
@@ -0,0 +1,10 @@
+package _java._se._01._start;
+
+public class Console {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
