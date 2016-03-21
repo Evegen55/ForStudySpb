dced6619d40539e8504f56c4557acfccbe6fa43a
Evegenn <demerbrise@gmail.com>
2016-03-21 17:29:25 +0300

    Loose accuracy example

---
+++ src/_java/_se/_01/_types/LooseAccuracy03.java
@@ -0,0 +1,14 @@
+package _java._se._01._types;
+
+public class LooseAccuracy03 {
+
+	public static void main(String[] args) {
+		long l1 = 1234567891234L;
+		float f1 = l1;
+		
+		System.out.println("l1 - " + l1);
+		System.out.println("f1 - " + f1);
+
+	}
+
+}
