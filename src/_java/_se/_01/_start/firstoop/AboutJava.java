07b99530eef98f121f638cdadfe03e47d251b27a
Evegenn <demerbrise@gmail.com>
2016-03-21 16:48:04 +0300

    first module

---
+++ src/_java/_se/_01/_start/firstoop/AboutJava.java
@@ -0,0 +1,9 @@
+package _java._se._01._start.firstoop;
+
+public class AboutJava {
+	
+	public void printReleaseData() {
+		System.out.println("Java уже здесь!");
+	}
+
+}
---
+++ src/_java/_se/_01/_start/firstoop/FirstOOPProgram.java
@@ -0,0 +1,9 @@
+package _java._se._01._start.firstoop;
+
+public class FirstOOPProgram {
+
+	public static void main(String[] args) {
+		AboutJava object = new AboutJava();
+		object.printReleaseData();
+	}
+}
