c8187b82f675c15536d766e866105e3ff78afb41
Evegenn <demerbrise@gmail.com>
2016-03-22 11:04:10 +0300

    Module1 task 1 work with command line

---
+++ src/javase01/t01/logic/Logic.java
@@ -0,0 +1,8 @@
+package javase01.t01.logic;
+
+public class Logic {
+	public String method() {
+		return "I am string in Logic";
+	}
+
+}
---
+++ src/javase01/t01/main/Main.java
@@ -0,0 +1,13 @@
+package javase01.t01.main;
+
+import javase01.t01.logic.Logic;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Logic logic = new Logic();
+		System.out.println(logic.method());
+
+	}
+
+}
