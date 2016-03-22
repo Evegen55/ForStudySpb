10729b158b0df985f6f0733d1651b67965c86932
Evegenn <demerbrise@gmail.com>
2016-03-22 13:34:51 +0300

    string to numbers

---
+++ src/_java/_se/_01/_operators/USRShift.java
@@ -0,0 +1,36 @@
+package _java._se._01._operators;
+
+public class USRShift {
+
+	public static void main(String[] args) {
+		int i = -1; //11111111111111111111111111111111
+		System.out.println(i);
+		i >>>= 10;  //00000000011111111111111111111111
+		System.out.println(i);
+		
+		long l = -1; 
+		System.out.println(l);
+		l >>>= 10;  
+		System.out.println(l);
+		
+		short s = -1; 
+		System.out.println(s);
+		s >>>= 10;  
+		System.out.println(s);
+		
+		byte b = -1; 
+		System.out.println(b);
+		b >>>= 10;  
+		System.out.println(b);
+		
+		int r = 3;
+		r = -r++ + r++ + -r;
+		System.out.println(r);
+		
+		int w = 3;
+		w = -w++;
+		System.out.println(r);
+
+	}
+
+}
