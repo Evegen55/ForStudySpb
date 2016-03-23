373f5b5c6f77158d83c231ffe928a931ec069b8f
Evegenn <demerbrise@gmail.com>
2016-03-23 13:49:30 +0300

    module 1 task 02

---
+++ src/_java/_se/_01/_task01/TestFindMin.java
@@ -0,0 +1,10 @@
+package _java._se._01._task01;
+
+public class TestFindMin {
+
+	public static void main(String[] args) {
+		FindMin fm = new FindMin(0.1);
+		fm.findSeriesDependsOnE();
+    }
+
+}
