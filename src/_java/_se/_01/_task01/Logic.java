7258cfc72fc18897c57dc72fccecf540f7169610
Evegenn <demerbrise@gmail.com>
2016-03-23 15:47:58 +0300

    add code for task 01

---
+++ src/_java/_se/_01/_task01/Logic.java
@@ -0,0 +1,10 @@
+package _java._se._01._task01;
+
+//package javase01.t01.logic;
+
+public class Logic {
+	public String method() {
+		return "I am string in Logic";
+	}
+
+}
---
+++ src/_java/_se/_01/_task01/Main.java
@@ -0,0 +1,15 @@
+package _java._se._01._task01;
+
+//package javase01.t01.main;
+
+import javase01.t01.logic.Logic;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Logic logic = new Logic();
+		System.out.println(logic.method());
+
+	}
+
+}
