1c5989d3ace2fdd8151fc4ffb5f489f44aadb1c9
Evegenn <demerbrise@gmail.com>
2016-03-24 16:47:50 +0300

    generics

---
+++ src/_java/_se/_02/_generics/Mark.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package _java._se._02._generics;
+
+/**
+ * @author Lartsev
+ *
+ */
+public class Mark<T extends Number> {
+	
+	public T mark;
+
+	/**
+	 * @param mark
+	 */
+	public Mark(T mark) {
+		this.mark = mark;
+	}
+
+	/**
+	 * @return the mark
+	 */
+	public T getMark() {
+		return mark;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int roundMark() {
+		return Math.round(mark.floatValue());
+	}
+	
+	/**
+	 * 
+	 * @param ob
+	 * @return
+	 */
+	public boolean sameAny(Mark<?> ob) {
+		return roundMark() == ob.roundMark();
+	}
+	
+	/**
+	 * 
+	 * @param ob
+	 * @return
+	 */
+	public boolean same(Mark<T> ob) {
+		return getMark() == ob.getMark();
+	}
+	
+
+}
---
+++ src/_java/_se/_02/_inheritance/Department.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package _java._se._02._inheritance;
+
+import java.util.Date;
+
+/**
+ * @author Lartsev
+ *
+ */
+public class Department implements Cloneable {
+	
+	private Integer name;
+	private Date date = new Date();
+	
+	/**
+	 * @return
+	 * @throws CloneNotSupportedException 
+	 * @see java.util.Date#clone()
+	 */
+	public Object clone() throws CloneNotSupportedException {
+		Department obj = null;
+		obj = (Department) super.clone();
+		
+		if (null != this.date) {
+			obj.date = (Date) this.date.clone();
+		}
+		
+		return obj;
+	}
+	
+	
+
+}
