8d7c207288f7750c1bee287666d899b6a13ab896
Evegenn <demerbrise@gmail.com>
2016-03-25 14:44:57 +0300

    inner classes

---
+++ src/_java/_se/_02/_innerclass/SimpleClass.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package _java._se._02._innerclass;
+
+/**
+ * @author Lartsev
+ *
+ */
+public class SimpleClass {
+	
+	public void print() {
+		System.out.println("This is print() from SimpleClass");
+	}
+
+}
