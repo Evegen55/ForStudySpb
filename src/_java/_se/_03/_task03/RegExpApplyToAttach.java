1d5fee4b787b682ead32fb7b3d1b638fda6626df
Evegenn <demerbrise@gmail.com>
2016-03-29 17:50:56 +0300

    JavaSE3 task 03 started

---
+++ src/_java/_se/_03/_task03/RegExpApplyToAttach.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package _java._se._03._task03;
+
+/**
+ * @author Lartsev
+ *
+ */
+public class RegExpApplyToAttach {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
