309d70bc00c48d12884ebc25cc653649e71e5ae3
Evegenn <demerbrise@gmail.com>
2016-03-30 11:41:30 +0300

    JavaSE3 task 1 started

---
+++ src/_java/_se/_03/_task01/CrazyLogger.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package _java._se._03._task01;
+
+/**
+ * @author Lartsev
+ *
+ */
+public class CrazyLogger {
+	
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
