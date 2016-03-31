9af5c828587463ca91b16f15b1fac2abfeb9ab61
Evegenn <demerbrise@gmail.com>
2016-03-31 12:20:10 +0300

    create first JUnit class (empty)

---
+++ src/tests/TestCaseExample.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Lartsev
+ *
+ */
+public class TestCaseExample {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
