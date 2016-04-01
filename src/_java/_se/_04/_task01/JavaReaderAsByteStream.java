47c0d97f77f5171eebd0dfc65c48fa3e575234a2
Evegenn <demerbrise@gmail.com>
2016-04-01 18:39:42 +0300

    working with JavaSE04 task01

--- src/_java/_se/_04/_task01/JavaReaderAsByteStream.java
+++ src/_java/_se/_04/_task01/JavaReaderAsByteStream.java
@@ -19,6 +19,17 @@ import java.io.IOException;
  */
 public class JavaReaderAsByteStream {
 
+	private String[] arr = {"abstract","continue","for","new","switch",
+			"assert","default","goto","package","synchronized",
+			"boolean","do","if","private","this",
+			"break","double","implements","protected","throw",
+			"byte","else","import","public","throws",
+			"case","enum","instanceof","return","transient",
+			"catch","extends","int","short","try",
+			"char","final","interface","static","void",
+			"class","finally","long","strictfp","volatile",
+			"const","float","native","super","while"};
+	
 	/**
 	 * 
 	 */
@@ -29,10 +40,27 @@ public class JavaReaderAsByteStream {
 				new FileInputStream(new File(fileName)));
 				BufferedOutputStream bfo = new BufferedOutputStream(
 						new FileOutputStream(new File(fileNameOut)))) {
+			StringBuffer sb = new StringBuffer();
 			while(bfi.available()>0) {
-				int b = bfi.read();
-				System.out.print( (char) b);
-				bfo.write(b);
+				int b =  bfi.read();
+				char c =(char) b;
+				//System.out.print(c);
+				//bfo.write(b);
+				
+				sb.append(c);
+				if(b == 32) {
+					String word = sb.toString();
+					//System.out.print(word);
+					for (String str : arr) {
+						if (word.equalsIgnoreCase(str)) {
+							System.out.print(word);
+						}
+					}
+					break;
+				}
+				
+				
+				
 			}
 				
 				
