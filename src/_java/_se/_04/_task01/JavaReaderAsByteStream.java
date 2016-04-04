50e83bd03c968519ad013468fce55d5a96db11fb
Evegenn <demerbrise@gmail.com>
2016-04-04 12:55:14 +0300

    JavaSe04 task01 has been completed

--- src/_java/_se/_04/_task01/JavaReaderAsByteStream.java
+++ src/_java/_se/_04/_task01/JavaReaderAsByteStream.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package _java._se._04._task01;
 
@@ -12,6 +12,10 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Lartsev
@@ -29,48 +33,78 @@ public class JavaReaderAsByteStream {
 			"char","final","interface","static","void",
 			"class","finally","long","strictfp","volatile",
 			"const","float","native","super","while"};
-	
+
 	/**
-	 * 
+	 *
 	 */
 	public void readFromFile() {
 		String fileName = "other/ForJavaReader.txt";
 		String fileNameOut = "other/ForJavaReaderOut.txt";
+
+		//working with only byte stream
 		try(BufferedInputStream bfi = new BufferedInputStream(
 				new FileInputStream(new File(fileName)));
 				BufferedOutputStream bfo = new BufferedOutputStream(
 						new FileOutputStream(new File(fileNameOut)))) {
+
+			//for adding single character to a word
 			StringBuffer sb = new StringBuffer();
+			//for count number of key words in the code
+			HashMap<String, Integer> mp = new HashMap<>();
+
 			while(bfi.available()>0) {
 				int b =  bfi.read();
 				char c =(char) b;
-				//System.out.print(c);
-				//bfo.write(b);
-				
 				sb.append(c);
-				if(b == 32) {
-					String word = sb.toString();
-					//System.out.print(word);
+				//only letters
+				if( b < 65 || (b > 90 & b < 97)) {
+					sb.deleteCharAt(sb.length()-1);
+					String word = sb.toString().trim();
 					for (String str : arr) {
-						if (word.equalsIgnoreCase(str)) {
-							System.out.print(word);
+						if (word.compareTo(str) == 0) {
+							if(!mp.containsKey(word)) {
+								mp.put(word, 1);
+							} else {
+								mp = addCountToHashMap(mp, word);
+							}
 						}
 					}
-					break;
+					sb = new StringBuffer();
 				}
-				
-				
-				
 			}
-				
-				
-				
+
+			printMap(mp);
+
+			//working with only byte stream
+			for (Map.Entry<String, Integer> entry : mp.entrySet()) {
+				String strOut = entry.getKey() + "\t" + entry.getValue() + "\n";
+				bfo.write(strOut.getBytes());
+			}
+
 		} catch (IOException e) {
-		// TODO Auto-generated catch block
 		e.printStackTrace();
 		}
 	}
-	
+
+	//a helper method for add 1 to a value in the map
+	private HashMap<String, Integer> addCountToHashMap(HashMap<String, Integer> mp, String word) {
+		for (Map.Entry<String, Integer> entry : mp.entrySet()) {
+			if (entry.getKey().compareTo(word) == 0) {
+				entry.setValue(entry.getValue() + 1);
+			}
+		}
+		return mp;
+	}
+
+
+	//a helper method for print map (for test)
+	private void printMap(Map<String, Integer> mp) {
+		for (Map.Entry<String, Integer> entry : mp.entrySet()) {
+			System.out.print( "key" + "\t" + entry.getKey() + "\t");
+			System.out.println( "value" + "\t" + entry.getValue());
+		}
+	}
+
 	/**
 	 * @param args
 	 */
