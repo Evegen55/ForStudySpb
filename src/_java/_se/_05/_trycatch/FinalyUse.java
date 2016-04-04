1b22c6e9ddbf3ca4c541047234d4fc6f5c7528c3
Evegenn <demerbrise@gmail.com>
2016-04-04 16:07:57 +0300

    works with try-catch

---
+++ src/_java/_se/_05/_trycatch/FinalyUse.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package _java._se._05._trycatch;
+
+/**
+ * @author Lartsev
+ *
+ */
+public class FinalyUse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
