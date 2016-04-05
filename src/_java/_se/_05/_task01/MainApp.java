27ab72ef0760f4d7c425bb8d4394b60f9588cf6b
Evegenn <demerbrise@gmail.com>
2016-04-05 16:28:51 +0300

    JavaSE 05 task 01 has been started

---
+++ src/_java/_se/_05/_task01/MainApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package _java._se._05._task01;
+
+/**
+ * @author Lartsev
+ *
+ */
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/_java/_se/_05/_task01/WorkWithTextFile.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package _java._se._05._task01;
+
+/**
+ * @author Lartsev
+ *
+ */
+public class WorkWithTextFile {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+        FileTreeList ftl = new FileTreeList();
+		
+		String[] str = ftl.getListUnderRootDisk();
+		ftl.printList(str);
+		
+		String[] str1 = ftl.getListUnderRootProject();
+		ftl.printList(str1);
+		
+		String[] str2 = ftl.getListUnderPathWithFilter("dghdf", ".*\\.java$");
+		ftl.printList(str2);
+		
+		String[] str3 = ftl.getListTextFileUnderPath("other");
+		ftl.printList(str3);
+		
+		String[] str4 = ftl.getListUnderPathWithFilter("src/_java/_se/_04/_iostream/", "fgfdh");
+		ftl.printList(str4);
+		
+		String[] str5 = ftl.getListUnderPathWithFilter("src/_java/_se/_04/_iostream/", ".*\\.java$");
+		ftl.printList(str5);
+		
+		String[] str6 = ftl.getListTextFileUnderPath("othenbvnbr");
+		ftl.printList(str6);
+
+	}
+
+}
