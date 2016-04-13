608e9f875d6f6bd24da731a6392509373e65e2c4
Evegenn <demerbrise@gmail.com>
2016-04-13 11:56:11 +0300

    multithreading in java. first example. create class and runb examoles

---
+++ src/_java/_se/_07_Multithreading/_echuprina/MainApp.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package _java._se._07_Multithreading._echuprina;
+
+/**
+ * @author Lartsev
+ *
+ */
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		OneThread ot = new OneThread();
+		System.out.println("\n" + "with multithreading ===========================" + "\n");
+		MultiThread mt = new MultiThread();
+
+	}
+
+}
