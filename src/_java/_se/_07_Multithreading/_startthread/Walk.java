c879c3634b61f7a8b5a32bb3a405866bdde77788
Evegenn <demerbrise@gmail.com>
2016-04-13 12:16:05 +0300

    multithreading in java. create class.

--- src/_java/_se/_07_Multithreading/_startthread/Walk.java
+++ src/_java/_se/_07_Multithreading/_startthread/Walk.java
@@ -14,7 +14,15 @@ public class Walk implements Runnable {
 	 */
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < 8; i++) {
+			System.out.println("Walking");
+			try {
+				Thread.sleep(400);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
 	}
 
