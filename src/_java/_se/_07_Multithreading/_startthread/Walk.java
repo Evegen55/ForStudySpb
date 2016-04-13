23985d0d3bd83974f61776af09128be8d780509b
Evegenn <demerbrise@gmail.com>
2016-04-13 11:56:34 +0300

    multithreading in java. first example from lesson. create class

---
+++ src/_java/_se/_07_Multithreading/_startthread/Walk.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package _java._se._07_Multithreading._startthread;
+
+/**
+ * @author Lartsev
+ *
+ */
+public class Walk implements Runnable {
+
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
