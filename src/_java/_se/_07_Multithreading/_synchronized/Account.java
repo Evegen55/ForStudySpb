09401e9beca5ba5e28c4b343cd7e850ed3c85250
Evegenn <demerbrise@gmail.com>
2016-04-14 16:22:28 +0300

    sinchronyzed threads. create account

---
+++ src/_java/_se/_07_Multithreading/_synchronized/Account.java
@@ -0,0 +1,52 @@
+/**
+ *
+ */
+package _java._se._07_Multithreading._synchronized;
+
+/**
+ * @author Lartsev
+ *
+ */
+public class Account {
+	private int balance;
+
+	/**
+	 * @param balance
+	 */
+	public Account(int balance) {
+		this.balance = balance;
+	}
+
+	/**
+	 * @return the balance
+	 */
+	public int getBalance() {
+		return balance;
+	}
+
+	/**
+	 *
+	 * @param amount
+	 */
+	public void deposit(int amount){
+		int x = balance + amount;
+		try {
+			Thread.sleep(15);
+		} catch (InterruptedException e) {
+			System.out.println("Exception in:");
+			e.printStackTrace();
+		}
+		balance = x;
+	}
+	public void withdraw(int amount) {
+		int x = balance - amount;
+		try {
+			Thread.sleep(20);
+		} catch (InterruptedException e) {
+			System.out.println("Exception in:");
+			e.printStackTrace();
+		}
+		balance = x;
+	}
+
+}
