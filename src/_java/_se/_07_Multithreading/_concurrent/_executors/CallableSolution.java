a2bb5fabfcc7b77bfce0a1a06235e45b9b9ac3c4
Evegenn <demerbrise@gmail.com>
2016-04-18 14:16:15 +0300

    Callable example....very hard to get...

---
+++ src/_java/_se/_07_Multithreading/_concurrent/_executors/CallableSolution.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package _java._se._07_Multithreading._concurrent._executors;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * @author Lartsev
+ *
+ */
+public class CallableSolution {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ExecutorService ex = Executors.newCachedThreadPool();
+		Future<Integer> s = ex.submit(new CallableThread());
+		Future<Integer> s1 = ex.submit(new CallableThread());
+		
+		try {
+			System.out.println("I'm here");
+			System.out.println(s.isDone());
+			System.out.println(s.get());
+			System.out.println(s1.get());
+			System.out.println(s1.isDone());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
