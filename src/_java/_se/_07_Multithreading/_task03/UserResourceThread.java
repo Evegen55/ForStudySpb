65e096b0034f6ef0a05087117de65cfae43b4d01
Evegenn <demerbrise@gmail.com>
2016-04-19 14:24:45 +0300

    multithreading. task03. created classes

---
+++ src/_java/_se/_07_Multithreading/_task03/UserResourceThread.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package _java._se._07_Multithreading._task03;
+
+/**
+ * @author Lartsev
+ *
+ */
+public class UserResourceThread {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		SharedResource res = new SharedResource();
+		IntegerSetterGetter t1 = new IntegerSetterGetter("1", res);
+		IntegerSetterGetter t2 = new IntegerSetterGetter("2", res);
+		IntegerSetterGetter t3 = new IntegerSetterGetter("3", res);
+		IntegerSetterGetter t4 = new IntegerSetterGetter("4", res);
+		IntegerSetterGetter t5 = new IntegerSetterGetter("5", res);
+
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		t5.start();
+
+		Thread.sleep(100);
+
+		t1.stopThread();
+		t2.stopThread();
+		t3.stopThread();
+		t4.stopThread();
+		t5.stopThread();
+
+		t1.join();
+		t2.join();
+		t3.join();
+		t4.join();
+		t5.join();
+
+		System.out.println("main");
+	}
+}
+
