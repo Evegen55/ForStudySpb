151e3899e8ccb4e80cbd55c12e2130b1e07b434b
Evegenn <demerbrise@gmail.com>
2016-04-21 17:14:44 +0300

    Java SE. JDBC. Creating connection pool. Create resource manager

---
+++ src/_java/_se/_08_JDBC/DBResourceManager.java
@@ -0,0 +1,33 @@
+/**
+ *
+ */
+package _java._se._08_JDBC;
+
+import java.util.ResourceBundle;
+
+/**
+ * @author Lartsev
+ *
+ */
+public class DBResourceManager {
+	private final static DBResourceManager instance = new DBResourceManager();
+
+	private ResourceBundle bundle = ResourceBundle
+			.getBundle("other/for_properties/db.properties");
+
+	/**
+	 * @return the instance
+	 */
+	public static DBResourceManager getInstance() {
+		return instance;
+	}
+
+	/**
+	 * @return the bundle
+	 */
+	public String getBundle(String key) {
+		return bundle.getString(key);
+	}
+
+
+}
