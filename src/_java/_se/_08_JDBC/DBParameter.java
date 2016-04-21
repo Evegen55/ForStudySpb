d938d58d99f4f904959d48464cb7146efd7a79ec
Evegenn <demerbrise@gmail.com>
2016-04-21 17:15:15 +0300

    Java SE. JDBC. Creating connection pool. Create parameter of the database

---
+++ src/_java/_se/_08_JDBC/DBParameter.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+package _java._se._08_JDBC;
+
+/**
+ * @author Lartsev
+ *
+ */
+public final class DBParameter {
+
+	public DBParameter() {}
+
+	public static final String DB_DRIVER = "db.driver";
+	public static final String DB_URL = "db.url";
+	public static final String DB_USER = "db.user";
+	public static final String DB_PASSWORD = "db.password";
+	public static final String DB_POOLSIZE = "db.poolsize";
+}
