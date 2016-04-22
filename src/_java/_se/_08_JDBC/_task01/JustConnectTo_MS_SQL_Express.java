dc358a3176a89de8c71824bc433e2884c1420569
Evegenn <demerbrise@gmail.com>
2016-04-22 14:52:19 +0300

    Java SE. JDBC. Task 01 has been completed successful

--- src/_java/_se/_08_JDBC/_task01/JustConnectTo_MS_SQL_Express.java
+++ src/_java/_se/_08_JDBC/_task01/JustConnectTo_MS_SQL_Express.java
@@ -18,6 +18,71 @@ import com.microsoft.sqlserver.jdbc.SQLServerException;
  *
  */
 public class JustConnectTo_MS_SQL_Express {
+	
+	private String query;
+	private String preparedQuery;
+	private String deleteQuery;
+	
+	
+	/**
+	 * @param query
+	 * @param preparedQuery
+	 * @param deleteQuery
+	 */
+	public JustConnectTo_MS_SQL_Express(String query, String preparedQuery, String deleteQuery) {
+		this.query = query;
+		this.preparedQuery = preparedQuery;
+		this.deleteQuery = deleteQuery;
+	}
+	
+	
+	/**
+	 * @return the query
+	 */
+	public String getQuery() {
+		return query;
+	}
+
+
+	/**
+	 * @param query the query to set
+	 */
+	public void setQuery(String query) {
+		this.query = query;
+	}
+
+
+	/**
+	 * @return the preparedQuery
+	 */
+	public String getPreparedQuery() {
+		return preparedQuery;
+	}
+
+
+	/**
+	 * @param preparedQuery the preparedQuery to set
+	 */
+	public void setPreparedQuery(String preparedQuery) {
+		this.preparedQuery = preparedQuery;
+	}
+
+
+	/**
+	 * @return the deleteQuery
+	 */
+	public String getDeleteQuery() {
+		return deleteQuery;
+	}
+
+
+	/**
+	 * @param deleteQuery the deleteQuery to set
+	 */
+	public void setDeleteQuery(String deleteQuery) {
+		this.deleteQuery = deleteQuery;
+	}
+
 
 	public void MyQuery(){
 		
@@ -25,7 +90,7 @@ public class JustConnectTo_MS_SQL_Express {
 		//Create connection poll using library by Microsoft
 		SQLServerConnectionPoolDataSource sds = new SQLServerConnectionPoolDataSource();
 		
-		sds.setServerName("");
+		sds.setServerName("127.0.0.1");
 		sds.setInstanceName("SQLEXPRESS");
         sds.setPortNumber(1433);
         sds.setDatabaseName("paymentsDB_v_1_3_SQLExpress");
@@ -35,12 +100,13 @@ public class JustConnectTo_MS_SQL_Express {
         sds.setDescription("just try it!");
         
         try(Connection con = sds.getConnection()) {
-        	//switch off autocommit
+        	
+        	//switch off autocommit mode
         	con.setAutoCommit(false);
         	
         	//create and execute query
-        	PreparedStatement stmt = con.prepareStatement("SELECT * FROM creditCards;");
-        	stmt.setString(1, "clientID");
+        	PreparedStatement stmt = con.prepareStatement(preparedQuery);
+        	stmt.setInt(1, 2);
         	ResultSet rs = stmt.executeQuery();
         	
         	//commit transaction
@@ -48,22 +114,52 @@ public class JustConnectTo_MS_SQL_Express {
         	
         	//show result executed query
         	while (rs.next()) {
-                System.out.println(rs.getString(1).trim() +"\t"+ rs.getString(2) +"\t"+ rs.getString(3) +"\t"+ rs.getString(4) );
+                System.out.println(rs.getString(1).trim() 
+                		+"\t"+ rs.getString(2) 
+                		+"\t"+ rs.getString(3) 
+                		+"\t"+ rs.getString(4)
+                		);
             }
         	
         	//turns off the transaction.
         	stmt.close(); 
+        	System.out.println("============================================");
+        	//create and execute query
+        	Statement stmt1 = con.createStatement();
+        	ResultSet rs1 = stmt1.executeQuery(query);
+        	
+        	//commit transaction
+        	con.commit();
+        	
+        	//show result executed query
+        	while (rs1.next()) {
+                System.out.println(rs1.getString(1).trim() 
+                		+"\t"+ rs1.getString(2).trim() 
+                		+"\t"+ rs1.getString(3).trim() 
+                		+"\t"+ rs1.getString(4).trim()
+                		);
+            }
+        	
+        	//turns off the transaction.
+        	stmt1.close();
+        	
+        	System.out.println("============================================");
+        	//create and execute query
+        	Statement stmt2 = con.createStatement();
+        	boolean rs2 = stmt2.execute(deleteQuery);
+        	
+        	//commit transaction
+        	con.commit();
         	
         	//read some database meta
         	DatabaseMetaData dmd = con.getMetaData();
-            System.out.println(dmd.getDatabaseProductName());
-            System.out.println(dmd.getDatabaseProductVersion());
-            System.out.println(dmd.getDefaultTransactionIsolation());
-            System.out.println(dmd.getDriverVersion());
-            System.out.println(dmd.getSQLKeywords()+"\n");
-            System.out.println(dmd.getURL()+"\n");
-            
-        	
+            System.out.println(dmd.getDatabaseProductName()
+            		+"\n" + dmd.getDatabaseProductVersion()
+            		+"\n" + dmd.getDefaultTransactionIsolation()
+            		+"\n" + dmd.getDriverVersion()
+            		+"\n" + dmd.getSQLKeywords()
+            		+"\n" + dmd.getURL()
+            		);
         } catch (SQLServerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -78,9 +174,12 @@ public class JustConnectTo_MS_SQL_Express {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		JustConnectTo_MS_SQL_Express c = new JustConnectTo_MS_SQL_Express();
+		String preparedQuery = "SELECT * FROM creditCards where client_ID = ?;";
+		String query = "SELECT * FROM bankAccount;";
+		String queryDelete = "drop table clients_groups;";
+		
+		JustConnectTo_MS_SQL_Express c = new JustConnectTo_MS_SQL_Express(query, preparedQuery, queryDelete);
 		c.MyQuery();
-
 	}
 
 }
