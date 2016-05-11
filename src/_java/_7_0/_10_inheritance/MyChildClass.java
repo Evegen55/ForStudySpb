80e14ad70e74bd896f2f9c1f33f9484f46e4b5e9
Evegenn <demerbrise@gmail.com>
2016-05-11 15:16:26 +0300

    create child class

---
+++ src/_java/_7_0/_10_inheritance/MyChildClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package _java._7_0._10_inheritance;
+
+/**
+ * @author Lartsev
+ *
+ */
+public class MyChildClass extends MyClass {
+
+	/**
+	 * 
+	 */
+	public MyChildClass() {
+		super.toString();
+	}
+
+}
