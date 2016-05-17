7677d357fd0b1327aff20d924616fb9ddb03146d
Evegenn <demerbrise@gmail.com>
2016-05-17 15:41:59 +0300

    ADT Optional<T>

---
+++ src/_java/_8_0/_DataAndTimeAPI/OptionalExamples.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package _java._8_0._DataAndTimeAPI;
+
+import java.util.Optional;
+
+/**
+ * @author Lartsev
+ *
+ */
+public class OptionalExamples {
+
+	/**
+	 * 
+	 */
+	public OptionalExamples() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * 
+	 * @param x
+	 * @return
+	 */
+	public static Optional<Double> inverse(Double x) {
+		return x == 0 ? Optional.empty() : Optional.of(1/x);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(inverse(12.0).get());
+		System.out.println(inverse(0.0).isPresent());
+	}
+
+}
