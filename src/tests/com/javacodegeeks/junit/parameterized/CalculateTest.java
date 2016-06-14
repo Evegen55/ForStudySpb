afb698adb056c8e4598872200cce1cdddf39f017
Evegenn <demerbrise@gmail.com>
2016-06-15 02:05:22 +0300

    add javadoc as description how we can use parameterized test

--- src/tests/com/javacodegeeks/junit/parameterized/CalculateTest.java
+++ src/tests/com/javacodegeeks/junit/parameterized/CalculateTest.java
@@ -22,16 +22,36 @@ public class CalculateTest {
 	private int first;
 	private int second;
 	
+	/*
+	 * The class has a single constructor that stores the test data.
+	 */
 	public CalculateTest(int expectedResult, int firstNumber, int secondNumber) {
 		this.expected = expectedResult;
 		this.first = firstNumber;
 		this.second = secondNumber;
 	}
+	/*
+	 * The class has a static method that generates and returns test data 
+	 * and is annotated with the @Parameters annotation.
+	 * 
+	 * Each array includes the inputs/output numbers of each test execution.
+	 * The number of elements in each array must be the same with the number of 
+	 * parameters in the constructor. So, in this specific case, each array
+	 * includes three elements, two elements that represent the numbers to be added
+	 * and one element for the result.
+	 */
 	@Parameters
 	public static Collection addedNumbers() {
-		return Arrays.asList(new Integer[][] { { 3, 1, 2 }, { 5, 2, 3 },
-		{ 7, 3, 4 }, { 9, 4, 5 }, });
+		return Arrays.asList(new Integer[][] { { 3, 1, 2 }, 
+											   { 5, 2, 3 },
+											   { 7, 3, 4 }, 
+											   { 9, 4, 5 }, 
+											});
 	}
+	/*
+	 * The class has a test, which obviously means that it needs a method 
+	 * annotated with the @Test annotation.
+	 */
 	@Test
 	public void sum() {
 		Calculate add = new Calculate();
@@ -39,4 +59,9 @@ public class CalculateTest {
 		+ second);
 		assertEquals(expected, add.sum(first, second));
 	}
+	
+	/*
+	 * As we see in the output, the test case is executed four times, 
+	 * which is the number of inputs in the method annotated with @Parameters annotation.
+	 */
 }
