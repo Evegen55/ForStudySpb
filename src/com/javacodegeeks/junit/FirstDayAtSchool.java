291565931dd9cd753a71e865288f6f3e5a809316
Evegenn <demerbrise@gmail.com>
2016-06-14 19:27:16 +0300

    simple JUnit test

--- src/com/javacodegeeks/junit/FirstDayAtSchool.java
+++ src/com/javacodegeeks/junit/FirstDayAtSchool.java
@@ -7,14 +7,14 @@ public class FirstDayAtSchool {
 	public String[] prepareMyBag() {
 		String[] schoolbag = { "Books", "Notebooks", "Pens" };
 		System.out.println("My school bag contains: "
-		+ Arrays.toString(schoolbag));
+				+ Arrays.toString(schoolbag));
 		return schoolbag;
 	}
 	
 	public String[] addPencils() {
 	String[] schoolbag = { "Books", "Notebooks", "Pens", "Pencils" };
 	System.out.println("Now my school bag contains: "
-	+ Arrays.toString(schoolbag));
+			+ Arrays.toString(schoolbag));
 	return schoolbag;
 	}
 }
--- src/tests/FirstDayAtSchoolTest.java
+++ src/tests/FirstDayAtSchoolTest.java
@@ -4,16 +4,26 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class FirstDayAtSchoolTest {
+import com.javacodegeeks.junit.FirstDayAtSchool;
 
+public class FirstDayAtSchoolTest {
+	
+	FirstDayAtSchool school = new FirstDayAtSchool();
+	String[] bag1 = { "Books", "Notebooks", "Pens" };
+	String[] bag2 = { "Books", "Notebooks", "Pens", "Pencils" };
+	
 	@Test
 	public void testPrepareMyBag() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		System.out.println("Inside testPrepareMyBag()");
+		assertArrayEquals(bag1, school.prepareMyBag());
 	}
 
 	@Test
 	public void testAddPencils() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		System.out.println("Inside testAddPencils()");
+		assertArrayEquals(bag2, school.addPencils());
 	}
 
 }
