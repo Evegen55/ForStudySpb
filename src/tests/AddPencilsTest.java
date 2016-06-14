9cb5b789757150d6b103d51fc38842bfdaca315e
Evegenn <demerbrise@gmail.com>
2016-06-14 23:46:28 +0300

    create test case for one method

---
+++ src/tests/AddPencilsTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddPencilsTest {
+
+	@Test
+	public void testAddPencils() {
+		fail("Not yet implemented");
+	}
+
+}
