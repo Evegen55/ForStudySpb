3046e686fcd0caef48d28ba232334c660191bbaf
Evegenn <demerbrise@gmail.com>
2016-06-14 14:42:02 +0300

    working with JUnit4

---
+++ src/com/javacodegeeks/junit/FirstDayAtSchool.java
@@ -0,0 +1,20 @@
+package com.javacodegeeks.junit;
+
+import java.util.Arrays;
+
+public class FirstDayAtSchool {
+	
+	public String[] prepareMyBag() {
+		String[] schoolbag = { "Books", "Notebooks", "Pens" };
+		System.out.println("My school bag contains: "
+		+ Arrays.toString(schoolbag));
+		return schoolbag;
+	}
+	
+	public String[] addPencils() {
+	String[] schoolbag = { "Books", "Notebooks", "Pens", "Pencils" };
+	System.out.println("Now my school bag contains: "
+	+ Arrays.toString(schoolbag));
+	return schoolbag;
+	}
+}
---
+++ src/tests/FirstDayAtSchoolTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FirstDayAtSchoolTest {
+
+	@Test
+	public void testPrepareMyBag() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddPencils() {
+		fail("Not yet implemented");
+	}
+
+}
