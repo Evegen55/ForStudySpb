e708339d6bfdfbf8c78d9d73ce2b081b51f1a19d
Evegenn <demerbrise@gmail.com>
2016-06-14 23:54:48 +0300

    With the @Suite.SuiteClasses annotation you can define which test classes will be included in the execution.

--- src/tests/SuiteTest.java
+++ src/tests/SuiteTest.java
@@ -4,6 +4,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ PrepareMyBagTest.class, AddPencilsTest.class })
+//With the @Suite.SuiteClasses annotation you can define which test classes 
+//will be included in the execution.
 public class SuiteTest {
 	
 }
