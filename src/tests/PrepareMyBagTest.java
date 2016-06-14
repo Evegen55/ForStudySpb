a0f131d8b29d025f0639116b7806ae6f863d3d25
Evegenn <demerbrise@gmail.com>
2016-06-14 23:46:50 +0300

    create test case for other method

---
+++ src/tests/PrepareMyBagTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PrepareMyBagTest {
+
+	@Test
+	public void testPrepareMyBag() {
+		fail("Not yet implemented");
+	}
+
+}
