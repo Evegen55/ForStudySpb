d4963e08dce20de074a9a213143f38e1d9bfb69a
Evegenn <demerbrise@gmail.com>
2016-06-15 02:11:43 +0300

    JavaLab HW create JUnit test template class for DerivedRange

---
+++ src/tests/_java_JavaLab/JUnit4/DerivedRangeTest.java
@@ -0,0 +1,91 @@
+/**
+ * 
+ */
+package tests._java_JavaLab.JUnit4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import _java_JavaLab.JUnit4.DerivedRange;
+import _java_JavaLab.JUnit4.Range;
+
+/**
+ * @author Evegen
+ *
+ */
+public class DerivedRangeTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link _java_JavaLab.JUnit4.DerivedRange#isBefore(_java_JavaLab.JUnit4.Range)}.
+	 */
+	@Test
+	public void testIsBefore() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link _java_JavaLab.JUnit4.DerivedRange#isAfter(_java_JavaLab.JUnit4.Range)}.
+	 */
+	@Test
+	public void testIsAfter() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link _java_JavaLab.JUnit4.DerivedRange#isConcurrent(_java_JavaLab.JUnit4.Range)}.
+	 */
+	@Test
+	public void testIsConcurrent() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link _java_JavaLab.JUnit4.DerivedRange#getLowerBound()}.
+	 */
+	@Test
+	public void testGetLowerBound() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link _java_JavaLab.JUnit4.DerivedRange#getUpperBound()}.
+	 */
+	@Test
+	public void testGetUpperBound() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link _java_JavaLab.JUnit4.DerivedRange#contains(long)}.
+	 */
+	@Test
+	public void testContains() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link _java_JavaLab.JUnit4.DerivedRange#asList()}.
+	 */
+	@Test
+	public void testAsList() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link _java_JavaLab.JUnit4.DerivedRange#asIterator()}.
+	 */
+	@Test
+	public void testAsIterator() {
+		fail("Not yet implemented");
+	}
+
+}
