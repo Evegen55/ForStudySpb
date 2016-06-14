45369cdb7f719a28ce9c85f02a1f91fee6ddc203
Evegenn <demerbrise@gmail.com>
2016-06-14 23:53:03 +0300

    A test suite is a collection of some test cases from different classes that can be run all together using @RunWith and @Suite annotations.
    The other test class will test the method addPencils().

--- src/tests/PrepareMyBagTest.java
+++ src/tests/PrepareMyBagTest.java
@@ -4,11 +4,18 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class PrepareMyBagTest {
+import com.javacodegeeks.junit.FirstDayAtSchool;
 
+public class PrepareMyBagTest {
+	
+	FirstDayAtSchool school = new FirstDayAtSchool();
+	String[] bag1 = { "Books", "Notebooks", "Pens" };
+	
 	@Test
 	public void testPrepareMyBag() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		System.out.println("Inside testPrepareMyBag()");
+		assertArrayEquals(bag1, school.prepareMyBag());
 	}
 
 }
