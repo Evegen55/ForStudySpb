6ab1aa0d7e3d00aa175e95c9c570be8bde12de8a
Evegenn <demerbrise@gmail.com>
2016-06-15 00:48:02 +0300

    create method sum

--- src/com/javacodegeeks/junit/Calculate.java
+++ src/com/javacodegeeks/junit/Calculate.java
@@ -2,4 +2,9 @@ package com.javacodegeeks.junit;
 
 public class Calculate {
 
+	public int sum(int var1, int var2) {
+		System.out.println("Adding values: " + var1 + " + " + var2);
+		return var1 + var2;
+	}
+
 }
