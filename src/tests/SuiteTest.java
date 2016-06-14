d0db435d41ff37278c241994d0e04e9198a69ce6
Evegenn <demerbrise@gmail.com>
2016-06-14 23:47:06 +0300

    create test suite

---
+++ src/tests/SuiteTest.java
@@ -0,0 +1,9 @@
+package tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ PrepareMyBagTest.class, AddPencilsTest.class })
+public class SuiteTest {
+	
+}
