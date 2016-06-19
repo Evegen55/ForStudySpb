7d3b683d106f77a1071bd0f4f981fc637d91d75f
Evegenn <demerbrise@gmail.com>
2016-06-19 13:24:32 +0300

    analyze code and rewrite as Java Code Convention

--- src/from_TIJ4/concurrency/shared_res/IntGenerator.java
+++ src/from_TIJ4/concurrency/shared_res/IntGenerator.java
@@ -2,9 +2,18 @@ package from_TIJ4.concurrency.shared_res;
 //: concurrency/IntGenerator.java
 
 public abstract class IntGenerator {
-  private volatile boolean canceled = false;
-  public abstract int next();
-  // Allow this to be canceled:
-  public void cancel() { canceled = true; }
-  public boolean isCanceled() { return canceled; }
+	
+	private volatile boolean canceled = false;
+	
+	public abstract int next();
+	
+	// Allow this to be canceled:
+	
+	public void cancel() { 
+		canceled = true; 
+	}
+	
+	public boolean isCanceled() { 
+		return canceled; 
+	}
 } ///:~
