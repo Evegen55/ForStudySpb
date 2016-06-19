a4761d56387c08ed84e073a82eec030e9759cef6
Evegenn <demerbrise@gmail.com>
2016-06-19 13:20:53 +0300

    copy class from book

---
+++ src/from_TIJ4/concurrency/shared_res/IntGenerator.java
@@ -0,0 +1,10 @@
+package from_TIJ4.concurrency.shared_res;
+//: concurrency/IntGenerator.java
+
+public abstract class IntGenerator {
+  private volatile boolean canceled = false;
+  public abstract int next();
+  // Allow this to be canceled:
+  public void cancel() { canceled = true; }
+  public boolean isCanceled() { return canceled; }
+} ///:~
