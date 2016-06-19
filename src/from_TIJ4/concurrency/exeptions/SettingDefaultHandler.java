0b64c7738ac080c6b97d847c9d313a84bc0da905
Evegenn <demerbrise@gmail.com>
2016-06-19 13:18:17 +0300

    copy class from book

---
+++ src/from_TIJ4/concurrency/exeptions/SettingDefaultHandler.java
@@ -0,0 +1,14 @@
+package from_TIJ4.concurrency.exeptions;
+//: concurrency/SettingDefaultHandler.java
+import java.util.concurrent.*;
+
+public class SettingDefaultHandler {
+  public static void main(String[] args) {
+    Thread.setDefaultUncaughtExceptionHandler(
+      new MyUncaughtExceptionHandler());
+    ExecutorService exec = Executors.newCachedThreadPool();
+    exec.execute(new ExceptionThread());
+  }
+} /* Output:
+caught java.lang.RuntimeException
+*///:~
