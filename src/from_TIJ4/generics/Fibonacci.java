759a7f677c137bf61c57adadbd2c319e1e6bb5e7
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2017-05-29 12:32:25 +0300

    quick ref

--- src/from_TIJ4/generics/Fibonacci.java
+++ src/from_TIJ4/generics/Fibonacci.java
@@ -1,17 +1,22 @@
 package from_TIJ4.generics;
 
 public class Fibonacci implements Generator<Integer> {
-  private int count = 0;
-  public Integer next() { return fib(count++); }
-  private int fib(int n) {
-    if(n < 2) return 1;
-    return fib(n-2) + fib(n-1);
-  }
-  public static void main(String[] args) {
-    Fibonacci gen = new Fibonacci();
-    for(int i = 0; i < 20; i++)
-      System.out.println(gen.next() + " ");
-  }
+    private int count = 0;
+
+    public Integer next() {
+        return fib(count++);
+    }
+
+    private int fib(int n) {
+        if (n < 2) return 1;
+        return fib(n - 2) + fib(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Fibonacci gen = new Fibonacci();
+        for (int i = 0; i < 20; i++)
+            System.out.println(gen.next() + " ");
+    }
 } /* Output:
 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584
 *///:~
